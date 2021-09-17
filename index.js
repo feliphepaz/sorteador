d891144110f0fda4fe1c7a47736e34ef4daf0787
feliphepaz <feliphe.s.paz@gmail.com>
2021-09-17 18:07:26 -0300

    salvando projeto

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-// IGQVJWRGRhTjJCRzd6X21RUFZA5VVEwYVVuOHdUbzd1S0Q3QlhzUkhUSFZADRmRXNXNsTzdtQk91aEx6cGZAIUjNMam1ZARnd3Mmp6alIxX0wzYVB5b3hyM3FFNlkzVEdobmtRelNJMWt3VnVDSDgtQ0ZAMSwZDZD
-//
 const btn = document.querySelector(".btn");
 const ul = document.querySelector(".ul");
 
+const token = 'EAALyCMbsjBMBAIcUcmUAfEbZBw35FtW4zpVxSyrOZAIBha9uNKAcLtp34b0ZBZArFC8fBgCD54Xd4aK490mdwS2MBpW6Jf8U9y2r74U8ZAulHkRKZBdGZCz26iNZCvKV6L67G3lVRniVSbEhrLfsb6y2aJ2DVtQPC0FNdBeEQcHwZBHUzQ9DlMXkMMEF8DEbn5ZCrcAuCZC9ZAhoAHJDtZAPIqIf1EGWsSeVlZAf5SGAQzfChZCxAZDZD';
+
 async function fetchApi() {
-  const response = await fetch(
-    "https://graph.facebook.com/v11.0/18007017787323916/comments?access_token=EAALyCMbsjBMBAIcUcmUAfEbZBw35FtW4zpVxSyrOZAIBha9uNKAcLtp34b0ZBZArFC8fBgCD54Xd4aK490mdwS2MBpW6Jf8U9y2r74U8ZAulHkRKZBdGZCz26iNZCvKV6L67G3lVRniVSbEhrLfsb6y2aJ2DVtQPC0FNdBeEQcHwZBHUzQ9DlMXkMMEF8DEbn5ZCrcAuCZC9ZAhoAHJDtZAPIqIf1EGWsSeVlZAf5SGAQzfChZCxAZDZD"
+  const responseAccount = await fetch(
+    `https://graph.facebook.com/v12.0/me/accounts?access_token=${token}`
   );
-  const resolve = await response.json();
-  resolve.data.forEach((txt) => {
-    const li = document.createElement("li");
-    li.innerText = txt.text;
-    ul.appendChild(li);
-  });
+  const resolveAccount = await responseAccount.json();
+  const codelari = resolveAccount.data[0].id;
+
+  const responseInstagram = await fetch(
+    `https://graph.facebook.com/v12.0/${codelari}?fields=instagram_business_account&access_token=${token}`
+  );
+  const resolveInstagram = await responseInstagram.json();
+  const codelariInstagram = resolveInstagram.instagram_business_account.id;
+
+  const responseMedia = await fetch(
+    `https://graph.facebook.com/v12.0/${codelariInstagram}?fields=media&access_token=${token}`
+  );
+  const resolveMedia = await responseMedia.json();
+  console.log(resolveMedia.media.data);
 }
 
 btn.addEventListener("click", fetchApi);
