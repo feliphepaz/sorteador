f16d97f46c1967d8bccd6262c3f49b3aed1dc3d4
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-09-20 00:04:02 -0300

    comentarios puxados

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
 const btn = document.querySelector(".btn");
-const ul = document.querySelector(".ul");
 
-const token = 'EAALyCMbsjBMBAIcUcmUAfEbZBw35FtW4zpVxSyrOZAIBha9uNKAcLtp34b0ZBZArFC8fBgCD54Xd4aK490mdwS2MBpW6Jf8U9y2r74U8ZAulHkRKZBdGZCz26iNZCvKV6L67G3lVRniVSbEhrLfsb6y2aJ2DVtQPC0FNdBeEQcHwZBHUzQ9DlMXkMMEF8DEbn5ZCrcAuCZC9ZAhoAHJDtZAPIqIf1EGWsSeVlZAf5SGAQzfChZCxAZDZD';
+const token = 'EAALyCMbsjBMBAIZBqyvLg5D8JfOZBWYZCruE4WBTBZBlwXwNBk5vwmGRHqMUcNGUnIQy2n4NZA7ce9p5hlkcYRO5V53SohDCwfKAg1gQOe90FVgVENRzoasd0rAYblUfTQpkIqUsDWLcXdFBsUjfR2HTBJRsCv9pZADQZBF2SLYYsZBs2kFeW6WO4GjZAAveMYVG0tTtShfEGbnH62EqP58JfNHvZBNjvozAgPxa9ZBXHevLAZDZD';
 
-async function fetchApi() {
+const tokenEB = 'IGQVJWYVc2eUVVRk01X3BoMGRMM1lVMTVDZAVBDTWlTUm9SQUhxNGR2aHlvQlVTQW5PbS1ZAOXJGZA1haNk1vTmRBS3RLTTR6QWFNX1ppbEQzY3pvNmpVckt2SkVPdWo5TkE1TkVSaVFDVmIwZA0p0NFVVagZDZD';
+
+async function fetchAPI() {
+  btn.classList.add('active');
+
+  // Get Account ID
   const responseAccount = await fetch(
     `https://graph.facebook.com/v12.0/me/accounts?access_token=${token}`
   );
   const resolveAccount = await responseAccount.json();
-  const codelari = resolveAccount.data[0].id;
+  const getAccount = resolveAccount.data[0].id;
 
-  const responseInstagram = await fetch(
-    `https://graph.facebook.com/v12.0/${codelari}?fields=instagram_business_account&access_token=${token}`
-  );
-  const resolveInstagram = await responseInstagram.json();
-  const codelariInstagram = resolveInstagram.instagram_business_account.id;
 
-  const responseMedia = await fetch(
-    `https://graph.facebook.com/v12.0/${codelariInstagram}?fields=media&access_token=${token}`
+  // Get Instagram Account ID
+  const responseInstagramAccount = await fetch(
+    `https://graph.facebook.com/v12.0/${getAccount}?fields=instagram_business_account&access_token=${token}`
   );
-  const resolveMedia = await responseMedia.json();
-  console.log(resolveMedia.media.data);
+  const resolveInstagramAccount = await responseInstagramAccount.json();
+  const getInstagramAccount = resolveInstagramAccount.instagram_business_account.id;
+
+  // Get Posts ID
+  const responsePosts = await fetch(
+    `https://graph.facebook.com/v12.0/${getInstagramAccount}?fields=media&access_token=${token}`
+  );
+  const resolvePosts = await responsePosts.json();
+  const getPosts = resolvePosts.media.data;
+
+  // Get Posts Images
+  const responseImages = await fetch(
+    `https://graph.instagram.com/me/media?access_token=${tokenEB}&fields=media_url,media_type,caption,permalink`
+  );
+  const resolveImages = await responseImages.json();
+  const getImages = resolveImages.data;
+
+  const posts = document.querySelector('.posts');
+  posts.style.display = 'block';
+  const imagesContainer = document.querySelector('.posts ul');
+
+  getImages.forEach((image) => {
+    const imgUrl = image.media_url;
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    li.appendChild(img);
+    img.src = imgUrl;
+    imagesContainer.appendChild(li);
+  })
+
+  const imgPosts = document.querySelectorAll('.posts ul li img');
+  imgPosts.forEach((img, imgId) => {
+    img.addEventListener('click', () => {
+      getAllComments(getPosts[imgId].id);
+    })
+  })
 }
 
-btn.addEventListener("click", fetchApi);
+async function buildComment(userComment, textComment) {
+  const responseUserComment = await fetch(`https://graph.facebook.com/v12.0/${userComment}?fields=username&access_token=${token}`);
+  const resolveUserComment = await responseUserComment.json();
+  const formatComment = `@${resolveUserComment.username}: ${textComment}`
+  console.log(formatComment);
+}
+
+async function getAllComments(postId) {
+  const responseComments = await fetch(`https://graph.facebook.com/v12.0/${postId}?fields=comments&access_token=${token}`);
+  const resolveComments = await responseComments.json();
+  const comments = resolveComments.comments.data;
+  comments.forEach(comment => {
+    buildComment(comment.id, comment.text);
+  })
+}
+
+btn.addEventListener("click", fetchAPI);
